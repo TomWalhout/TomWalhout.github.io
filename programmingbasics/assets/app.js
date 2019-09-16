b5b6bfc5f4650c7d4750eeed95b20b30b55813c3
Tom <twalhout@gmail.com>
2019-09-16 14:14:05 +0200

    Worked on the exercises week 3

--- programmingbasics/assets/app.js
+++ programmingbasics/assets/app.js
@@ -14,19 +14,22 @@ function even(getal) {
 
 
 function deleteString(input) {
-    let pos = input.indexOf(" not");
-    input = input.slice(0, pos) + input.slice(pos + 4, input.length);
+    input = input.replace(" not", "");
     console.log(input);
 }
 
 
 function compare(num, str) {
-    //Comparing a number to a string doesn't work very well because they're different datatypes.
-    //"1400" is not the same as 1400 and definitely not the same as "Ik woon in Naboo".
+    /* Comparing a number to a string doesn't work very well because they're different datatypes.
+    "1400" is not the same as 1400 and definitely not the same as "Ik woon in Naboo".
+    If you made 1400 a string and you add them together you'd get "1400Ik woon in Naboo". \
+    If you made the string a number you'd get NaN. 
+    Long story short, don't compare different datatypes with each other. 
+    (Maybe use === to ensure the datatypes are the same)*/
     if (num == str) {
-        console.log("Yup :)");
+        console.log(`Yup, ${num} is hetzelfde als ${str}`);
     } else {
-        console.log("Nup :(");
+        console.log(`Nope, "${num}" is niet hetzelfde als "${str}"`);
     }
 }
 even(50);
