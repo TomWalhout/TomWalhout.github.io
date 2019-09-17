55327929bf00d8775bfe329ed51d37f2724d595e
Tom <twalhout@gmail.com>
2019-09-17 10:36:35 +0200

    Finished les4

--- programmingbasics/les4/assets/app.js
+++ programmingbasics/les4/assets/app.js
@@ -35,31 +35,39 @@ if (currentAction == "Calculating") {
     console.log("lol no idea...");
 }
 
+//Var voor resultaat
 let result;
 console.log("Met if statements:");
+//laat alle mogelijkheden zien
 for (let i = 0; i < 10; i += 0.1) {
     console.log(`${i} is ${resultCalc(i)}`);
 
 }
 
+//Als het cijfer hoger is dan 9 dan uitmuntend, tussen 7 en 9 dan goed etc...
 function resultCalc(cijfer) {
     if (cijfer >= 9) {
         result = "uitmuntend";
-    } else if (cijfer >= 7 && cijfer < 9) {
+    } else if (cijfer >= 7 && cijfer < 9) { //&& < cijfer 9 is eigenlijk overbodig omdat in dat geval de bovenste statement gepakt wordt 
         result = "goed";
     } else if (cijfer >= 6 && cijfer < 7) {
         result = "voldoende";
     } else if (cijfer < 6) {
         result = "onvoldoende";
     }
+    //Geef resultaat terug
     return result;
 }
+
+//Zelfde opdracht maar met switch
 console.log("Met switch:");
+//Laat alle resultaten zien
 for (let i = 0; i < 10; i++) {
     console.log(`${i} is ${switchResult(i)}`);
 }
 
-
+//Deze werkt alleen met ints want 100 cases is te veel. 
+//Kijk wat het cijfer is en geef het bijbehoorende resultaat
 function switchResult(cijfer) {
     switch (cijfer) {
         case 0:
@@ -82,14 +90,17 @@ function switchResult(cijfer) {
             result = "uitmuntend";
             break;
     }
+    //Geef het resultaat terug
     return result;
 }
 
 
+//Init vars
 let purchasedBook = true;
 let job = "teacher";
 let inTrain = false;
 
+//Check for combinations of vars and give the corresponding comment.
 if (purchasedBook && inTrain && job == "teacher") {
     console.log("Finally I can enjoy my book");
 } else if (purchasedBook && job == "teacher") {
